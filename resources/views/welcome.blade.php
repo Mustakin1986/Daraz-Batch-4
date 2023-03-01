e58bebc210c85a3eef264e2242f09d122b39c821
nuralam33 <nur.alamreza33@gmail.com>
2023-03-01 23:55:29 +0600

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Daraz</title>
+        <title>Daraz-2</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
