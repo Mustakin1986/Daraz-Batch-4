173e250675ef821f9e01cb1b0ec20f0c16cd3c42
Muntasir-Pranto <fahimmuntasir43@gmail.com>
2023-03-05 00:15:40 +0600

    color route is added

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-
+Route::get('/colors',[ColorController::class,'show']);
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
